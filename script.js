7519e1845808bc2badad72619a33d2e2e86f0c11
francisco Aguiar <francisco@gmail.com>
2022-09-07 20:00:34 -0400

    reogonizando

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensagem = document.querySelector(".mensagem");
-document.getElementById("div-aparece").style.display ='none';
+
 
 
 // Entrada de dados
 function btnEncriptar(){
     const textoEncriptado = encriptar(inputTexto.value)
-    mensagem.value = textoEncriptado
-    document.style.backgroundImage = "none"
-    aparece()
+    mensagem.value = textoEncriptado;
 }
 
 
@@ -27,37 +25,45 @@ function encriptar(stringEncriptada){
 
 
 // saida de dados
-
 function btnDesencriptar(){
-    const textoDesencriptar = desencriptar(inputTexto.value);
+    const textoEncriptado = desencriptar(inputTexto.value)
     mensagem.value = textoEncriptado;
-    /*mensagem.style.backgroundImage= 'none';
-    aparece(); */
-    
 }
+//[["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
 
-function btnDesencriptar(stringDesencriptar){
-    let matrizCodigo = [["e","enter"], ["i", "imes"],["a", "ai"], ["o", "ober"],["u", "uft"]];
-    stringDesencriptar = stringDesencriptar.toLowerCase();
+function desencriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"], ["i", "imes"], ["u", "ft"], ["o", "ober"], ["a", "ai"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
 
     for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringDesencriptar.includes(matrizCodigo[i][1])){
-            stringDesencriptar = stringDesencriptar.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        if(stringEncriptada.includes(matrizCodigo[i][1])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
 
         }
     }
-    return stringDesencriptar;
+    return stringEncriptada;
+    
 }
 
 //botão copiar
 
+function copiar() {
+
+    var mensagem = document.querySelector(".mensagem");
+    mensagem.select();
+
+    navigator.clipboard.writeText(mensagem.value);
+    mensagem.value = "";
+}
+
+
+
+
+/*
 function copiar(){
     const msg = document.querySelector(".mensagem");
     msg.ariaSelected();
     navigator.clipboard.writeText(msg.value);
     msg.value = "";
 }
-
-function aparece(){
-    document.getElementById("div-aparece").style.display = 'block';
-}
+*/
